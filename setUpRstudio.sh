815b968b0f6a8e9135e1993cb2048a45bc4acb44
ntjames <nj1154@gmail.com>
2017-07-20 00:07:57 -0500

    test push

--- setUpRstudio.sh
+++ setUpRstudio.sh
@@ -6,7 +6,7 @@ gpg --keyserver keyserver.ubuntu.com --recv-key E084DAB9
 gpg -a --export E084DAB9 | sudo apt-key add -
 
 ## note added r-dev
-sudo apt-get update
+sudo apt-get update 
 sudo apt-get -y install r-base r-base-dev
 
 ## add swap space to install stuff
